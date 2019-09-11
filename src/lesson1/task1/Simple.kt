ca0e418267bc3ca4bcee4ba1115b6d9a60bc7051
galievaregina <regina-galieva-1996@mail.ru>
2019-09-11 20:40:41 +0300

    Решены задачи

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -68,7 +68,8 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = 3600 * hours + 60 * m
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double =4.445 / 100 * (48 * sagenes + 16 * arshins + vershoks)
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double =
+    4.445 / 100 * (48 * sagenes + 16 * arshins + vershoks)
 
 
 /**
@@ -85,7 +86,7 @@ fun angleInRadian(deg: Int, min: Int, sec: Int): Double = (sec / 3600.0 + min / 
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqrt(sqr(x2-x1)+sqr(y2-y1))
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqrt(sqr(x2 - x1) + sqr(y2 - y1))
 
 /**
  * Простая
@@ -102,7 +103,8 @@ fun thirdDigit(number: Int): Int = (number / 100) % 10
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = (60 * hoursArrive + minutesArrive) - (60 * hoursDepart + minutesDepart)
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int =
+    (60 * hoursArrive + minutesArrive) - (60 * hoursDepart + minutesDepart)
 
 /**
  * Простая
